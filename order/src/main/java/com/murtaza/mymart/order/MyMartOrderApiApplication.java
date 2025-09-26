09fa3d20a3cafc8bda1138303a31a69dac6c3083
mecoder110 <er.murtaza110@gmail.com>
2025-09-26 21:43:30 +0530

    order-api

---
+++ order/src/main/java/com/murtaza/mymart/order/MyMartOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.murtaza.mymart.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMartOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMartOrderApiApplication.class, args);
+	}
+
+}
---
+++ order/src/test/java/com/murtaza/mymart/order/MyMartOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murtaza.mymart.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMartOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
